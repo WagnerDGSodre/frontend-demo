d2c5fb7f0e62b460a99cebb8d427efb2aded43b2
Hans Schedel <hans.haugush@philips.com>
2022-11-03 17:55:29 -0300

    [SO-00001] feat(add patient fields)

--- src/pages/patient.tsx
+++ src/pages/patient.tsx
@@ -1,61 +1,95 @@
-
 import { useState } from "react"
 import InputComponent from "../components/theme/InputComponent"
 import Layout from "../components/theme/Layout"
 
 export default function Patient(props) {
-    const [name, setName] = useState('')
-    const [cpf, setCpf] = useState('')
-    const [email, setEmail] = useState('')
-    const [endOpt, setOpt] = useState<'comercial'|'residencial'>('residencial')
+  const [endOpt, setOpt] = useState<'comercial' | 'residencial'>('residencial')
+  const [numberOfAddresses, setNumberOfAddresses] = useState(1)
+  const [patient, setPatient] = useState({
+    name: '',
+    cpf: '',
+    email: '',
+    birthdate: '',
+    address: [],
+  })
 
-    function enderecoOption(){
-        if(endOpt=='comercial'){
-         setOpt(endOpt == 'comercial' ? 'comercial' : 'residencial' )
-        }
-        setOpt(endOpt == 'residencial' ? 'comercial' : 'residencial' )
-        console.log(endOpt)
-    }
+  function enderecoOption() {
+    setOpt(endOpt === 'residencial' ? 'comercial' : 'residencial')
+    console.log(endOpt)
+  }
 
-
-    return (
-        <Layout title="Cadastro de pacientes">
-            <div className={`flex flex-col`}>
-                <div className="flex flex-col">
-                    <h3 className={`flex items-center justify-center`} >Preencha o formulário abaixo</h3>
-                    <label className={`flex items-center justify-center`}>Dados pessoais</label>
-                </div>
-                <div className={`border border-gray-400 m-5 flex flex-wrap gap-2 grid-cols-12 items-center w-11/12`}>                 
-                    <InputComponent label="Name" type="text" changeValue={setName} value={name} height={`10`} widht={`2/3`}></InputComponent>
-                    <InputComponent label="Cpf" type="text" value={cpf} changeValue={setCpf} height={`10`} widht={`1/3`}></InputComponent>
-                    <InputComponent label="Email" type="email" value={email} changeValue={setEmail} height={`10`} widht={`1/3`}></InputComponent>
-                </div>
+  console.log('patient ->', patient)
 
 
-                <div className={`flex w-screen`} >
+  return (
+    <Layout title="Cadastro de pacientes">
+      <div className={`flex flex-col`}>
+        <div className="flex flex-col">
+          <h3 className={`flex items-center justify-center`} >Preencha o formulário abaixo</h3>
+          <label className={`flex items-center justify-center`}>Dados pessoais</label>
+        </div>
 
-                    <button onClick={enderecoOption}  className={`w-screen border`}>
-                      {endOpt == 'residencial'?'comercial':"comercial" }
-                    
-                    </button>
+        <div className={`border border-gray-400 m-5 flex flex-col flex-wrap gap-2 grid-cols-12 items-start w-11/12`}>
+          <InputComponent
+            label="Nome Completo"
+            placeholder="Nome completo"
+            type="text"
+            changeValue={value => setPatient({ ...patient, name: value })}
+            value={patient.name}
+            height={`10`}
+            widht={`2/3`}
+          />
+          <InputComponent
+            label="CPF"
+            placeholder="000.000.000-00"
+            type="text"
+            value={patient.cpf}
+            changeValue={value => setPatient({ ...patient, cpf: value })}
+            height={`10`}
+            widht={`1/3`}
+          />
+          <InputComponent
+            label="E-Mail"
+            placeholder="email@exemplo.com"
+            type="email"
+            value={patient.email}
+            changeValue={value => setPatient({ ...patient, email: value })}
+            height={`10`}
+            widht={`1/3`}
+          />
+          <InputComponent
+            label="Data de nascimento"
+            placeholder="DD/MM/YYYY, DD-MM-YYYY"
+            type="text"
+            value={patient.birthdate}
+            changeValue={value => setPatient({ ...patient, birthdate: value })}
+            height={`10`}
+            widht={`1/3`}
+          />
 
-                
-                <div className={`border border-gray-400 m-5 flex flex-wrap gap-2 grid-cols-12 items-center w-11/12`}>                 
-                    <InputComponent label="Endereço"
-                     type="text" changeValue={setName}
-                      value={name}
-                       height={`10`}
-                       widht={`2/3`}
-                      ></InputComponent>
-                   
-                </div>
-                </div>
-            </div>
+          {Array(numberOfAddresses).map(number => 
+            <InputComponent
+              key={`address-${number}`}
+              label={`Endereço ${number}`}
+              placeholder="Rua Exemplo, 123"
+              type="text"
+              value={patient.address[number]}
+              changeValue={value => {
+                patient.address[number] = value
+              }}
+              height={`10`}
+              widht={`2/3`}
+            />
+          )}
+        </div>
 
+        <div className={`flex gap-2 m-5 grid-cols-12 w-11/12`} >
+          <button onClick={enderecoOption} className={`w-full border`}>
+            {endOpt === 'residencial' ? 'comercial' : "comercial"}
+          </button>
+        </div>
+      </div>
 
-
-        </Layout>
-
-
-    )
+    </Layout>
+  )
 }
