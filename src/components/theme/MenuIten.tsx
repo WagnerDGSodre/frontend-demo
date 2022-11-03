ac2bde489db8ccae940e17728292d24b44847cb3
Hans Schedel <hans.haugush@philips.com>
2022-11-03 17:54:21 -0300

    [SO-00001] fix(layout)

--- src/components/theme/MenuIten.tsx
+++ src/components/theme/MenuIten.tsx
@@ -1,21 +1,19 @@
 import Link from "next/link"
 
-interface propsMenuIten{
-    title:string,
-    url?:string,
-    icon?:any
+interface propsMenuIten {
+  title: string,
+  url?: string,
+  icon?: any
 }
 
-export default function MenuIten(props:propsMenuIten){
-    return(
-        <Link href={props.url} legacyBehavior>
-        <li className={` mr-3  mt-0 p-6 flex flex-col items-center w-full
-        h-xs hover:bg-slate-200`}>
-            {props.icon}
-            {props.title} 
-
-              
-        </li>
-        </Link>
-    )
+export default function MenuIten(props: propsMenuIten) {
+  return (
+    <Link href={props.url} legacyBehavior>
+      <li className={` mr-3  mt-0 p-6 flex flex-col items-center w-full
+        h-xs hover:bg-slate-200 hover:cursor-pointer`}>
+        {props.icon}
+        {props.title}
+      </li>
+    </Link>
+  )
 }
