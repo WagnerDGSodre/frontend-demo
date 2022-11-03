908cc1e19618d0e00483662824872b1fb998cdce
Hans Schedel <hans.haugush@philips.com>
2022-11-03 17:53:43 -0300

    [SO-00001] feat(add label and placeholder input)

--- src/components/theme/InputComponent.tsx
+++ src/components/theme/InputComponent.tsx
@@ -1,36 +1,37 @@
 interface inputPropsAuth {
-    label: string,
-    value: any,
-    required?: boolean
-    type: 'email' | 'text' | 'password' | 'textarea',
-    modeRender?: boolean,
-    changeValue?: (newValue: any) => void,
-    height: string,
-    widht: string
-    sm?:string,
-    md?:string,
-    lg?:string,
-    xl?:string,
+  label?: string,
+  placeholder: string,
+  value: any,
+  required?: boolean
+  type: 'email' | 'text' | 'password' | 'textarea',
+  modeRender?: boolean,
+  changeValue?: (newValue: any) => void,
+  height: string,
+  widht: string
+  sm?: string,
+  md?: string,
+  lg?: string,
+  xl?: string,
 
 
 }
 
 
 export default function InputComponent(props: inputPropsAuth) {
-    return props.modeRender == false ? null :
-        (              
-               
-                    <input
-                        placeholder={props.label}
-                        type={props.type}
-                        value={props.value}
-                        required={props.required}
-                        onChange={e => props.changeValue?.(e.target.value)}
-                        className={`w-${props.widht} h-${props.height}
+  return props.modeRender === false ? null :
+    (
+      <>
+        {props.label && <label className="p-2 m-2">{props.label}</label>}
+        <input
+          placeholder={props.placeholder}
+          type={props.type}
+          value={props.value}
+          required={props.required}
+          onChange={e => props.changeValue?.(e.target.value)}
+          className={`w-${props.widht} h-${props.height}
                         sm:w-${props.sm}  md:w-${props.md} lg:w-${props.lg}  xl:w-${props.xl}
                          p-2 m-2 border-blue-300 bg-slate-100 focus:bg-white `}
-                    />
-              
-           
-        )
+        />
+      </>
+    )
 }
